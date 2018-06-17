d367250aba3d4ecef639418bd5167832fcb1d2f8
Grant Rettke <gcr@wisdomandwonder.com>
2018-06-17 11:52:00 -0500

    Adds version and package-version meta

--- affect-vs-effect.el
+++ affect-vs-effect.el
@@ -5,6 +5,9 @@
 ;; Author: Grant Rettke <gcr@wisdomandwonder.com>
 ;; Keywords: languages
 
+;; Version: 1.0
+;; Package-Version: 1.0
+
 ;; This program is free software; you can redistribute it and/or modify
 ;; it under the terms of the GNU General Public License as published by
 ;; the Free Software Foundation, either version 3 of the License, or
